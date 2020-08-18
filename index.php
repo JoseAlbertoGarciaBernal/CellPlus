6a184e3c11504973c9f8cb53b9651d49d5614121
JoseAlbertoGarciaBernal <69213263+JoseAlbertoGarciaBernal@users.noreply.github.com>
2020-08-17 19:48:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-<html>
- <head>
-  <title>Prueba de PHP dos</title>
- </head>
- <body>
- <?php echo '<p>Hola Mundo</p>'; ?>
- </body>
- <h2>HOLA SOY MAURO</h2>
- <h2>HOLA soy jose</h2>
- <h2>HUEVOS A TODOS</h2>
- <h2>HUEVOS A TODOS x2</h2>
-
-</html>
+<?php 
+//redireccionar a la vista de login
+ 
+header('location: vistas/login.html');
+ ?>
